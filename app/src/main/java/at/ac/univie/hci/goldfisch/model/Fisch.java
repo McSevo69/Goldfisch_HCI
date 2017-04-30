ac3fc6e24b7b15e422388b749306c04e14c77789
Gerhard Schmidt <a1363761@unet.univie.ac.at>
2017-04-30 15:07:34 +0200

    Konstruktor test

--- app/src/main/java/at/ac/univie/hci/goldfisch/model/Fisch.java
+++ app/src/main/java/at/ac/univie/hci/goldfisch/model/Fisch.java
@@ -1,11 +1,28 @@
 package at.ac.univie.hci.goldfisch.model;
 
+import android.media.Image;
+
+import java.util.UUID;
+
+
+
 /**
  * Created by Gerhard on 30.04.2017.
  */
 
 public class Fisch {
 
-    
+    UUID id;
+    String name;
+    Image bild;
+    boolean imGlas;
+
+    public Fisch(String name, Image bild, boolean imGlas){
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.bild = bild;
+        this.imGlas = imGlas;
+    }
+
 
 }
