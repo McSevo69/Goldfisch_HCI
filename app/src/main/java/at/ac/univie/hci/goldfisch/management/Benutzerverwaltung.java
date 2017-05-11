8ebe81639805f474a0a569a71f8a6a228efe01ac
Gerhard Schmidt <a1363761@unet.univie.ac.at>
2017-05-11 15:47:20 +0200

    letztes

--- app/src/main/java/at/ac/univie/hci/goldfisch/management/Benutzerverwaltung.java
+++ app/src/main/java/at/ac/univie/hci/goldfisch/management/Benutzerverwaltung.java
@@ -205,7 +205,8 @@ public class Benutzerverwaltung implements Verwaltung {
 
         for(Status s : alleStati){
             if(testIfSameDay(s.getDatum(),heute)){
-                s.setTagesIstMenge(s.getTagesIstMenge()+b.getEffektiveTrinkmenge());
+                double tagesist = s.getTagesIstMenge();
+                s.setTagesIstMenge(tagesist+b.getEffektiveTrinkmenge());
                 statusHeuteVorhanden = true;
             }
         }
